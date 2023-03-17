2fd751b28670569450c41440d5ca418d0bb0e679
David256 <wasterdavid@gmail.com>
2023-03-17 07:53:39 -0500

    Export types

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
 export {
-  Html2PdfCerts    
+  Html2PdfCerts,
 } from './components/html2pdf-certs/Html2PdfCerts'
+
+export type {
+  Html2PdfCertsRef,
+} from './components/html2pdf-certs/Html2PdfCerts'
+
+export type {
+  CertRow,
+} from './components/html2pdf-certs/types'
