769d29ab8f4df2f48689f56d06bd2e226b85e1b6
David256 <wasterdavid@gmail.com>
2023-03-17 12:03:49 -0500

    Update dist

--- dist/main.js
+++ dist/main.js
@@ -19,7 +19,7 @@ const Html2PdfCerts = (props) => {
             margin,
             filename,
             image: { type: 'jpeg', quality: 1.0 },
-            html2canvas: { scale: 2 },
+            html2canvas: { scale: 2, useCORS: true },
             enableLinks,
             jsPDF: { unit, format, orientation },
             // jsPDF:        { unit: 'in', format: 'letter', orientation: 'landscape' }
--- dist/types/stories/Html2PdfCerts.stories.d.ts
+++ dist/types/stories/Html2PdfCerts.stories.d.ts
@@ -3,4 +3,5 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 declare const _default: ComponentMeta<import("react").FunctionComponent<import("../components/html2pdf-certs/Html2PdfCerts").IHtml2PdfCertsProps>>;
 export default _default;
 export declare const SimpleExample: ComponentStory<import("react").FunctionComponent<import("../components/html2pdf-certs/Html2PdfCerts").IHtml2PdfCertsProps>>;
+export declare const ExternalURLExample: ComponentStory<import("react").FunctionComponent<import("../components/html2pdf-certs/Html2PdfCerts").IHtml2PdfCertsProps>>;
 export declare const BasicExample: ComponentStory<import("react").FunctionComponent<import("../components/html2pdf-certs/Html2PdfCerts").IHtml2PdfCertsProps>>;
